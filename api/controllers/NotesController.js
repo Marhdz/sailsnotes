12a1bdfe3e96d25e922e61dbbbeb7c5ae0c3936f
Michelle Romero <marhdz0@gmail.com>
2018-08-02 21:37:01 -0600

    User model

--- api/controllers/NotesController.js
+++ api/controllers/NotesController.js
@@ -6,6 +6,54 @@
  */
 
 module.exports = {
-	
-};
+	list:function(req, res){
+      Notes.find({}).exec(function(err, notes){
+          if(err){
+              res.send(500, {error: 'Database Error'});
+          }
+          res.redirect('/notes', {notes:notes});
+      });
 
+  },
+  add: function(req, res){
+      res.view('add');
+  },
+  create:function(req, res){
+      var title = req.body.title;
+      var body = req.body.body;
+
+      Notes.create({title:title, body:body}).exec(function(err){
+          if(err){
+              res.send(500, {error: 'Database Error'});
+          }
+
+          res.redirect('/notes');
+      });
+  },
+  delete: function(req, res){
+      Notes.destroy({id:req.params.id}).exec(function(err){
+          if(err){
+              res.send(500, {error: 'Database Error'});
+          }
+
+          res.redirect('/notes');
+      });
+
+      return false;
+  },
+
+  update: function(req, res){
+      var title = req.body.title;
+      var body = req.body.body;
+	      Notes.update({id: req.params.id},{checked: true}).exec(function(err){
+	          if(err){
+	              res.send(500, {error: 'Database Error'});
+	          }
+
+	          res.redirect('/notes');
+	      });
+
+      return false;
+  }
+
+};
